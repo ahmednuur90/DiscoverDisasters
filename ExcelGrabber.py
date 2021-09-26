34cafb9087f4ef90118620065f6ceed038e75d67
Faisal Bagalagel <uultradude@gmail.com>
2021-09-26 00:49:49 -0400

    created findLocationDisasters function.

--- ExcelGrabber.py
+++ ExcelGrabber.py
@@ -1,33 +1,47 @@
 # Reading an excel file using Python
-import xlrd
 import csv
 
-# Give the location of the file
-# loc = "DisasterDataSet.csv"
-
-# To open Workbook
-# wb = xlrd.open_workbook(loc, encoding_override="utf-8")
-# sheet = wb.sheet_by_index(0)
-
-# For row 0 and column 0
-# print(sheet.cell_value(0, 0))
-
-# Step 1: Make file object
+# codes for convenience
+COUNTRY = 1
+COUNTRYCODE = 2
+WARDCODE = 3
+YEAR = 4
+GEOID = 5
+GEOLOCATION = 6
+LEVEL = 7
+LOCATION = 11
+DISASTER = 14
+DISASTERID = 15
+LATITUDE = 16
+LONGITUDE = 17
 
 f = open('DisasterDataSet.csv', encoding="utf8")
 
 # Step 2: Make csv reader object
 
-csv_reader_object = csv.reader(f)
+# csv_reader_object = csv.reader(f)
 
 
 # Step 3: Loop through rows
 
 def findCountryDisasters(country):
-    print("Natural Disasters in country of: "+country)
+    f = open('DisasterDataSet.csv', encoding="utf8")
+    csv_reader_object = csv.reader(f)
+    print("Natural Disasters in country of: " + country)
     for line in csv_reader_object:
         if line[1] == country:
-            print("["+line[14]+"] location: ["+line[6]+"] year: ["+line[4]+"]")
+            print("[" + line[14] + "] location: [" + line[6] + "] year: [" + line[4] + "]")
 
 
+def findLocationDisaters(location):
+    f = open('DisasterDataSet.csv', encoding="utf8")
+    csv_reader_object = csv.reader(f)
+    print("Natural Disasters in location of: " + location)
+    for line in csv_reader_object:
+        if line[6] == location:
+            print("[" + line[14] + "] country: [" + line[1] + "] year: [" + line[4] + "]")
+
+
+findLocationDisaters("Toronto")
+print()
 findCountryDisasters("Canada")
